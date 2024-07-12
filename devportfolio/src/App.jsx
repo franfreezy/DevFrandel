837d526fde454eeafe9376cec83526e1c035355f
franfreezy <frandelwanjawa@yahoo.com>
2024-07-12 12:32:01 +0300

    Configuring dark mode with tailwind, class toggles manually.

--- devportfolio/src/App.jsx
+++ devportfolio/src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div class="bg-white dark:bg-gray-800">
+  <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
+  <p class="text-gray-600 dark:text-gray-300">
+    Lorem ipsum...
+  </p>
+</div>
   )
 }
